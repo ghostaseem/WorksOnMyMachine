71ac7c52415c93729c8d45cf638a5acbd35e0abc
Daniel Kirkwood <danielkirkwood1973@gmail.com>
2023-10-11 22:47:22 +0100

    add case rotes

--- backend/routers/staff.py
+++ backend/routers/staff.py
@@ -1,19 +1,33 @@
 from fastapi import APIRouter
 
+from backend.db import connect
+
 router = APIRouter()
 
-staffs=[{"staff_id": "Foo","role":"admin"},{"staff_id":"Bar","role":"Therapist"}]
 
-@router.get("/staffs")
-async def read_staffs():
-    return staffs
+@router.get("/staff")
+async def read_staff():
+    conn = connect.connect_to_db()
+    cursor = conn.cursor()
 
-@router.get("/staffs/role/{role}")
+    cursor.execute("SELECT * FROM staff")
+    results = cursor.fetchall()
+    return results
+
+@router.get("/staff/role/{role}")
 async def read_staff_role(role:str):
-    return list(filter(lambda staff: staff["role"] == role,staffs))
+    conn = connect.connect_to_db()
+    cursor = conn.cursor()
 
-@router.get("/staffs/staff_id/{staff_id}")
+    cursor.execute("SELECT * FROM staff WHERE staff_role = %s", (role,))
+    results = cursor.fetchall()
+    return results
+
+@router.get("/staff/staff_id/{staff_id}")
 async def read_staff_id(staff_id: str):
-    print(staffs)
-    print(staff_id)
-    return list(filter(lambda staff: staff["staff_id"] == staff_id,staffs))
+    conn = connect.connect_to_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM staff WHERE staff_id = %s", (staff_id,))
+    results = cursor.fetchone()
+    return results
