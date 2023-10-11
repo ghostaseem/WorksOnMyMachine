bc4ddc30a4de7d96367966f46d736877dd1cb72c
Katie McConkey <klmcconkey@aol.com>
2023-10-11 21:51:47 +0100

    chat-component

--- UI/listening-ear-chat-tool/src/app/app.component.ts
+++ UI/listening-ear-chat-tool/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'listening-ear-chat-tool';
+  text = "Open"
+  isChatOpen = false;
+
+  openChat() {
+    if(this.isChatOpen == false){
+      this.isChatOpen = true;
+      this.text = "Close"
+    }
+    else{
+      this.isChatOpen = false;
+      this.text = "Open"
+    }
+    
+  }
 }
